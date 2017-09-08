4659809d5842e7c54939ae9c0025efe5b57393ef
Jonathan Sue <hello@jonosue.com>
2017-09-08 22:08:06 +0000

    Changed app.js file to use const instead of var

--- server/app.js
+++ server/app.js
@@ -2,21 +2,21 @@
 
 require('dotenv').config();
 
-var express = require('express');
+const express = require('express');
 const ENV = process.env.ENV || "development";
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const knexConfig = require("./knexfile");
 const knex = require("knex")(knexConfig[ENV]);
 const knexLogger  = require('knex-logger');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,7 +35,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
