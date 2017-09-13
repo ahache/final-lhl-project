65d8c1373aebd9809f93264e93cf517f7e8bc5bb
hounslow <hounslowmatthew@gmail.com>
2017-09-13 00:53:48 +0000

    Added request dependency

--- server/routes/map.js
+++ server/routes/map.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const request = require('request');
 
 router.post("/", (req, res) => {
   console.log("in map");
   const destination = req.body.destination;
-  res.json(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${destination}&key=AIzaSyBR9a87huIRF93xhp5VcW57S7mBjfFGEKk`);
-})
+  const URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${destination}&key=AIzaSyBR9a87huIRF93xhp5VcW57S7mBjfFGEKk`
+  request(URL, function (err, response, body) {
+    if (err || response.statusCode !== 200) {
+      return res.sendStatus(500);
+    } else {
+      console.log(body.results);
+      // for (result of body['results']) {
+      //   console.log(result.geometry.location);
+      // }
+    }
+  });
+});
 
 module.exports = router;
 // module.exports = () => {
