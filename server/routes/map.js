6f1a382bbc577d4a923f95f5f14b5e5727a314ae
Alex Hache <ahache8@gmail.com>
2017-09-14 02:55:24 +0000

    Built async functions in map.js to send gmap query data back to client

--- server/routes/map.js
+++ server/routes/map.js
@@ -10,31 +10,80 @@ const mapsClient = require('@google/maps').createClient({
 
 module.exports = (knex) => {
 
-  router.post("/", (req, res) => {
-
-    const decoded = jwt.verify(req.body.user, 'CBFC');
-    const user_id = decoded.user;
-    const { destination, radius } = req.body;
-    console.log(radius);
-
-    const URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${destination}&key=${API}`
-
-    request(URL, function (err, response, body) {
-      if (err || response.statusCode !== 200) {
-        return res.sendStatus(500);
-      } else {
-        const latLong = JSON.parse(body).results[0].geometry.location;
-        const latLongArr = [latLong.lat, latLong.lng];
-        mapsClient.places({query: 'tacos', radius: 500, type: 'restaurant', location: latLongArr}, (err, response) => {
-          if (!err) {
-            console.log('response');
-            console.log(response.json.results);
-          } else {
-            console.log(err);
-          }
-        });
-      }
+  const getGeocodePromise = (destination) => {
+    return new Promise((resolve, reject) => {
+      mapsClient.geocode({address: destination}, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          const latLong = response.json.results[0].geometry.location;
+          const latLongArr = [latLong.lat, latLong.lng];
+          resolve(latLongArr);
+        }
+      });
     });
+  }
+
+  async function getGeocode(destination) {
+    const latLong = await getGeocodePromise(destination);
+    return latLong;
+  }
+
+  const getFiltersPromise = (user_id) => {
+    return new Promise((resolve, reject) => {
+      knex('users_filters')
+        .join('filters', 'filters.id', '=', 'users_filters.filter_id')
+        .join('users', 'users.id', '=', 'users_filters.user_id')
+        .select('filters.name')
+        .where('users.id', '=', user_id)
+        .then((filters) => {
+          resolve(filters);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async function getFilters(user_id) {
+    const filters = await getFiltersPromise(user_id);
+    return filters;
+  }
+
+  const getPlacesPromise = (filter, latLong, radius) => {
+    return new Promise((resolve, reject) => {
+      mapsClient.places({query: filter.name, location: latLong, radius: Number(radius)}, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response.json.results);
+        }
+      });
+    });
+  }
+
+  async function getPlaces(filter, latLong, radius) {
+    const places = await getPlacesPromise(filter, latLong, radius);
+    return places;
+  }
+
+  router.post("/", async (req, res) => {
+
+    const { destination, radius, user } = req.body;
+    const decoded = jwt.verify(user, 'CBFC');
+    const user_id = decoded.user;
+    let mapResults = {};
+
+    const latLong = await getGeocode(destination);
+
+    const filters = await getFilters(user_id);
+
+    for (filter of filters) {
+      const places = await getPlaces(filter, latLong, radius);
+      mapResults[filter.name] = places;
+    }
+
+    res.json(mapResults);
   });
 
   return router;
