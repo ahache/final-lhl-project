c0c19883db49d1eea93e3f336cf53eada47b63f6
MattWillcox <mattjwillcox@gmail.com>
2017-09-14 17:21:08 -0700

    Inserts favorites into DB on add to favorites click

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -7,23 +7,56 @@ module.exports = (knex) => {
   router.get("/", (req, res) => {
     const decoded = jwt.verify(req.query.user, 'CBFC');
     const user = decoded.user;
-    const location = req.query.location;
+    const location = req.query.place_id;
     knex
       .from('users_favorites')
       .join('favorites', 'favorites.id', '=', 'users_favorites.favorite_id')
       .countDistinct('users_favorites.user_id')
       .where('users_favorites.user_id', '=', user)
-      .andWhere('favorites.id', '=', location)
+      .andWhere('favorites.place_id', '=', location)
       .then((results) => {
         res.json(results);
       });
   })
 
   router.post("/add", (req, res) => {
-    console.log("Success!");
-    res.json({works: true});
-  });
-
+    const decoded = jwt.verify(req.body.user, 'CBFC');
+    const user_id = decoded.user;
+    knex('favorites')
+    .select('id')
+    .where('place_id', '=', req.body.place_id)
+    .then((result) => {
+      if (result.length === 0) {
+        knex('favorites')
+        .returning('id')
+        .insert({
+          place_id: req.body.place_id,
+          address: req.body.address,
+          name: req.body.name,
+          price_level: req.body.price_level,
+          rating: req.body.rating,
+          query: req.body.query,
+          latitude: req.body.latitude,
+          longitude: req.body.longitude
+        })
+        .then((id) => {
+          favorite_id = id[0];
+          knex('users_favorites')
+          .insert({user_id: user_id, favorite_id: favorite_id})
+          .then(() => res.status(200).send("Added favorite for user"))
+          .catch(() => res.status(400).send("Failed to add fav for user"));
+        })
+        .catch(() => res.status(400).send("Failed to add fav"))
+        } else {
+          favorite_id = result[0];
+          knex('users_favorites')
+          .insert({user_id: user_id, favorite_id: favorite_id})
+          .then(() => res.status(200).send("Added favorite for user"))
+          .catch(() => res.status(400).send("User already has favorite"));
+        }
+      })
+      .catch(() => res.status(400).send("Error accessing favorites"))
+  })
 
   router.post("/remove", (req, res) => {
     console.log("Success!");
