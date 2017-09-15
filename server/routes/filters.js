0d8f69caaf6bf6ea7caf5ad9e3fa29f0c948e2b9
Alex Hache <ahache8@gmail.com>
2017-09-15 02:51:29 +0000

    Finished add/remove functionality for filters

--- server/routes/filters.js
+++ server/routes/filters.js
@@ -109,65 +109,17 @@ module.exports = (knex) => {
     return filt_id;
   }
 
-  // const insertIntoFiltersPromise = (user_id, filter)=> {
-  //   return new Promise((resolve, reject) => {
-  //     knex('filters')
-  //     .returning('id')
-  //     .insert({name: filter})
-  //     .then((id) => {
-  //       resolve(id);
-  //       filter_id = id[0];
-  //       knex('users_filters')
-  //       .insert({user_id: user_id, filter_id: filter_id})
-  //       .then((results) => {
-  //         resolve(results);
-  //       })
-  //       .catch((error) => {
-  //         reject(error);
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       reject(error);
-  //     });
-  //   });
-  // }
-  //
-  // async function insertIntoFilters(user_id, filter) {
-  //   const returned_id = await insertIntoFiltersPromise(user_id, filter);
-  //   return returned_id;
-  // }
-  //
-  // const insertIntoUserFiltersPromise = (user_id, filter, filter_id) => {
-  //   return new Promise((resolve, reject) => {
-  //     knex('users_filters')
-  //     .insert({user_id: user_id, filter_id: filter_id})
-  //     .then((results) => {
-  //       resolve(results);
-  //     })
-  //     .catch((error) => {
-  //       reject(error);
-  //     });
-  //   });
-  // }
-  //
-  // async function insertIntoUserFilters(user_id, filter, filter_id) {
-  //   const returned_id = await insertIntoUserFiltersPromise(user_id, filter, filter_id);
-  //   return returned_id;
-  // }\
-
   router.delete('/:id', (req, res) => {
     const user_id = jwt.verify(req.body.user, 'CBFC').user;
     knex('users_filters').where('filter_id', req.params.id).andWhere('user_id', user_id).del()
     .then((results) => {
       knex('users_filters')
         .join('filters', 'filters.id', '=', 'users_filters.filter_id')
-        .select('filters.name')
+        .select(['filters.name', 'filters.id'])
         .where('users_filters.user_id', user_id)
         .then((filters) => {
           res.json(filters);
         });
-      // const filters = await getFilters(user_id);
-      // res.json(filters);
     }).catch((error) => {
       console.log(error);
     });
@@ -205,51 +157,6 @@ module.exports = (knex) => {
     }
 
     res.json(filt_id);
-
-    // const retrievedId = await getFilterId(filter);
-    //
-    // if (retrievedId) {
-    //   const filterInsert = await insertIntoUserFilters(user_id, filter, filter_id);
-    // } else {
-    //   const filterInsert = await insertIntoFilters(user_id, filter);
-    // }
-
-    // let filter_id;
-    // if (filter) {
-
-    //   knex('filters')
-    //   .select('id')
-    //   .where('name', '=', filter)
-    //   .then((result) => {
-
-    //     if (result.length === 0) {
-
-    //       knex('filters')
-    //       .returning('id')
-    //       .insert({name: filter})
-    //       .then((id) => {
-
-    //         filter_id = id[0];
-
-    //         knex('users_filters')
-    //         .insert({user_id: user_id, filter_id: filter_id})
-    //         .then((result) => res.json(filter_id))
-    //         .catch(() => res.status(400).send("Failed to add filter for user"));
-    //       })
-
-    //       .catch(() => res.status(400).send("Failed to add filter"));
-    //     } else {
-    //       filter_id = result[0];
-    //       knex('users_filters')
-    //       .insert({user_id: user_id, filter_id: filter_id})
-    //       .then(() => res.status(200).send("Added filter for user"))
-    //       .catch(() => res.status(400).send("User already has filter"));
-    //     }
-    //   })
-    //   .catch(() => res.status(400).send("Error accessing filters"))
-    // } else {
-    //   res.status(400).send("No filter in request");
-    // }
   });
 
   return router;
