317be8c037a39fa0b4777b596e6f73b276fe201e
hounslow <hounslowmatthew@gmail.com>
2017-09-16 10:19:06 -0700

    Made some styling adjustments and added a dropdown with redirects

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
-import {Link, Redirect } from 'react-router-dom';
+import {Link, Redirect, Router} from 'react-router-dom';
 import Logout from './Logout.jsx'
+import $ from 'jquery'
 
 class NavBar extends Component {
+  constructor(props){
+    super(props)
+    this.transitionToUser = this.transitionToUser.bind(this);
+    this.dropDown = this.dropDown.bind(this);
+    this.transitionToMap = this.transitionToMap.bind(this);
+  }
+
+  transitionToUser() {
+    var transitionTo = Router.transitionTo;
+    transitionTo('/user' );
+  }
+
+  transitionToMap() {
+    var transitionTo = Router.transitionTo;
+    transitionTo('/user' );
+  }
+
+  dropDown(e) {
+    $(".navbar-item.has-dropdown").toggleClass('is-active');
+  }
+
   render() {
     if (localStorage.getItem('token')) {
       return (
-        <nav>
-        🏠
-        <Logout />
-        <Link to='/user'>Update Profile</Link>
+        <nav className="navbar is-light" role="navigation" aria-label="dropdown navigation">
+          <div className="navbar-item has-dropdown">
+            <a className="navbar-link" onClick={this.dropDown}>
+              Menu
+            </a>
+
+            <div className="navbar-dropdown">
+              <div className="navbar-item">
+                <Link to={`/map`} >Map</Link>
+              </div>
+              <hr class="navbar-divider" />
+
+              <div className="navbar-item">
+                <Link to={`/filters`} >Filters</Link>
+              </div>
+              <hr class="navbar-divider" />
+
+              <div className="navbar-item">
+                <Link to={`/user`} >Update Profile</Link>
+              </div>
+              <hr class="navbar-divider" />
+              <Logout />
+            </div>
+          </div>
         </nav>
       );
     } else {
       return (
         <nav>
-        🏠
+          🏠
         </nav>
       );
     }
