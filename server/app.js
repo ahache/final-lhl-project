1e9e74a536904f51c62761aadc34d148d4621fc3
hounslow <hounslowmatthew@gmail.com>
2017-09-19 12:47:35 -0700

    removed build folder

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 // app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('./build'));
 } else {
   app.use(express.static(path.join(__dirname, 'public')));
 }
