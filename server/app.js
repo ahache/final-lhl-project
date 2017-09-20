30db5090a4c14516b8193fab58aeac0629fd0f1c
hounslow <hounslowmatthew@gmail.com>
2017-09-20 13:06:01 -0700

    Made it so app uses headers

--- server/app.js
+++ server/app.js
@@ -32,6 +32,13 @@ app.use(methodOverride('_method'));
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
